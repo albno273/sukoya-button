429851f596109103d0f30f840732c979fe363525
Yuki Umeda <100minus1birds.equal.swan@gmail.com>
2019-10-17 07:13:16 +0900

    [update] change build root

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,16 @@
 const colors = require('vuetify/es5/util/colors').default
 
+const routerBase =
+  process.env.DEPLOY_ENV === 'GH_PAGES'
+    ? {
+        router: {
+          base: '/<repository-name>/'
+        }
+      }
+    : {}
+
 module.exports = {
+  ...routerBase,
   mode: 'spa',
   /*
    ** Headers of the page
