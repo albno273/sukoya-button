c4eb15c31a35f53a043bb715a8c97ebfba0528c8
Yuki Umeda <100minus1birds.equal.swan@gmail.com>
2019-10-21 02:05:04 +0900

    [fix] 外部リンクが効かない問題を修正

--- components/Information.vue
+++ components/Information.vue
@@ -2,20 +2,19 @@
   <div class="mt-3 mb-5">
     <v-card outlined>
       <v-card-text>
-        健屋さんからいつも元気をもらっています。
-        <br />勝手に作ってすみません。
+        健屋さんからいつも元気をもらっています。<br />
+        勝手に作ってすみません。<br />
         <br />
-        <br />よかったら宣伝してください。
-        <twitter-share />
+        よかったら宣伝してください。
+        <twitter-share /><br />
+        問い合わせや音声の追加依頼はこちらにお願いします: <a :href="gitHubIssueUrl" target_="blank">GitHub issue</a> or
+        <a :href="twitterUrl" target="_blank">Twitter</a>
       </v-card-text>
       <v-divider />
       <v-card-title>オマージュ / 参考 / 先駆者さま</v-card-title>
       <v-card-text>
-        <a src="https://www.natorisana.love/" target="_blank">さなボタン(2)</a> さま<br />
-        <a src="https://wikiwiki.jp/nijisanji/%E2%97%8B%E2%97%8B%E3%83%9C%E3%82%BF%E3%83%B3"
-          >にじさんじ所属ライバー ボタン</a
-        >
-        製作者の皆さま
+        <a :href="sanaButtonUrl" target="_blank">さなボタン(2)</a> さま<br />
+        <a :href="nijiSanjiWikiButtonUrl" target="_blank">にじさんじ所属ライバー ボタン</a> 製作者の皆さま
       </v-card-text>
     </v-card>
   </div>
@@ -29,5 +28,11 @@ import TwitterShare from './TwitterShare.vue';
     TwitterShare,
   },
 })
-export default class Information extends Vue {}
+export default class Information extends Vue {
+  /* data */
+  sanaButtonUrl = 'https://www.natorisana.love/';
+  nijiSanjiWikiButtonUrl = 'https://wikiwiki.jp/nijisanji/%E2%97%8B%E2%97%8B%E3%83%9C%E3%82%BF%E3%83%B3';
+  gitHubIssueUrl = 'https://github.com/albno273/sukoya-button/issues';
+  twitterUrl = 'https://twitter.com/albNo273';
+}
 </script>
