f9db7851773f7d3d2cc369630f5a22aa7f982902
Yuki Umeda <100minus1birds.equal.swan@gmail.com>
2019-10-21 14:40:56 +0900

    [update] 音声データのオブジェクトを別ファイルに移した(管理方法考えたほうがよさそう)

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div>
     <information />
     <broadcast
-      v-for="broadcast in data.broadcasts"
+      v-for="broadcast in voices.broadcasts"
       :key="broadcast.link"
       :date="broadcast.date"
       :title="broadcast.title"
@@ -15,7 +15,7 @@
 
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator';
-import { VoiceData } from '../types/index';
+import { voices } from '../voice';
 import Information from '../components/Information.vue';
 import Broadcast from '../components/Broadcast.vue';
 import Profile from '../components/Profile.vue';
@@ -29,21 +29,6 @@ import Profile from '../components/Profile.vue';
 })
 export default class App extends Vue {
   /* data */
-  data: VoiceData = {
-    broadcasts: [
-      {
-        title:
-          '【９万人記念】ケーキ食べて少しだけ歌いました、皆さんいつもありがとうございます！【健屋花那/にじさんじ】',
-        date: '20191020',
-        link: 'https://www.youtube.com/watch?v=OgO6ghgBSLo',
-        quotes: [
-          {
-            quote: 'brrrr',
-            time: '00:41',
-          },
-        ],
-      },
-    ],
-  };
+  voices = voices;
 }
 </script>
---
+++ voice.ts
@@ -0,0 +1,17 @@
+import { VoiceData } from './types/index';
+
+export const voices: VoiceData = {
+  broadcasts: [
+    {
+      title: '【９万人記念】ケーキ食べて少しだけ歌いました、皆さんいつもありがとうございます！【健屋花那/にじさんじ】',
+      date: '20191020',
+      link: 'https://www.youtube.com/watch?v=OgO6ghgBSLo',
+      quotes: [
+        {
+          quote: 'brrrr',
+          time: '00:41',
+        },
+      ],
+    },
+  ],
+};
