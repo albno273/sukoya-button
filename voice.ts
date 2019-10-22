77e0037413a7f37347ac158081bcb52afa5a4aad
Yuki Umeda <100minus1birds.equal.swan@gmail.com>
2019-10-23 03:39:08 +0900

    [add] 2019/10/22 配信分のボタンを追加

--- voice.ts
+++ voice.ts
@@ -37,5 +37,120 @@ export const voices: VoiceData = {
         },
       ],
     },
+    {
+      title: '【Dark Deception】サバイバーの修行リベンジします【健屋花那/にじさんじ】',
+      date: '20191022',
+      link: 'https://www.youtube.com/watch?v=S-_DAzpreVQ',
+      quotes: [
+        {
+          quote: '悲鳴01',
+          time: '07:16',
+        },
+        {
+          quote: '悲鳴02',
+          time: '08:11',
+        },
+        {
+          quote: '悲鳴03',
+          time: '10:12',
+        },
+        {
+          quote: '悲鳴04',
+          time: '11:13',
+        },
+        {
+          quote: '悲鳴05',
+          time: '12:14',
+        },
+        {
+          quote: '悲鳴06',
+          time: '14:18',
+        },
+        {
+          quote: '悲鳴07',
+          time: '16:00',
+        },
+        {
+          quote: 'スタンしろや！',
+          time: '16:52',
+        },
+        {
+          quote: '悲鳴08',
+          time: '19:37',
+        },
+        {
+          quote: '悲鳴09',
+          time: '20:00',
+        },
+        {
+          quote: '悲鳴10',
+          time: '21:55',
+        },
+        {
+          quote: '悲鳴11',
+          time: '22:39',
+        },
+        {
+          quote: '英語に強い健屋',
+          time: '22:58',
+        },
+        {
+          quote: '悲鳴12',
+          time: '24:30',
+        },
+        {
+          quote: '悲鳴13',
+          time: '25:03',
+        },
+        {
+          quote: '悲鳴14',
+          time: '25:25',
+        },
+        {
+          quote: '悲鳴15',
+          time: '27:53',
+        },
+        {
+          quote: '悲鳴16',
+          time: '30:11',
+        },
+        {
+          quote: '悲鳴17',
+          time: '40:11',
+        },
+        {
+          quote: '悲鳴18',
+          time: '43:11',
+        },
+        {
+          quote: '悲鳴19',
+          time: '45:24',
+        },
+        {
+          quote: '悲鳴20',
+          time: '49:22',
+        },
+        {
+          quote: '悲鳴21',
+          time: '55:45',
+        },
+        {
+          quote: '普段はもっといい子なんですぅ',
+          time: '57:15',
+        },
+        {
+          quote: '死ぬんじゃない生きるんだ',
+          time: '58:19',
+        },
+        {
+          quote: '悲鳴22',
+          time: '58:42',
+        },
+        {
+          quote: '悲鳴23',
+          time: '1:00:32',
+        },
+      ],
+    },
   ],
 };
